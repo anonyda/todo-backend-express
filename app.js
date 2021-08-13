38b87a11eed2cbda542df5f955df57e6eb4d9676
Nida <zaynida123@gmail.com>
2021-08-13 10:31:02 +0530

    added response headers for CORS

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: false}));
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 })
 
